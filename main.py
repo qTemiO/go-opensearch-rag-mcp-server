a3d3499569460d87d64324d4e5c2fc96c0cb95eb
qTemiO <71713499+qTemiO@users.noreply.github.com>
2025-11-07 16:39:16 +0300

    test(feat):
    * added test time feature for testing in gorag service

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
+from datetime import datetime
+
+from loguru import logger
 from fastmcp import FastMCP
 
 mcp = FastMCP(name="Gorag MCP Server")
 
 
 @mcp.tool
-def greet(name: str) -> str:
-    return f"Hello, {name}!"
+def current_time() -> datetime:
+    """
+    Current time in user location 
+    """
+    logger.success("Current time called")
+    return datetime.now()
+
+
+@mcp.tool
+def enchant_prompt(prompt: str) -> str:
+    """
+    Enchants prompt for better generation
+    """
+    logger.success("Enchant prompt called")
+    return f"New prompt: {prompt}!"
 
 
 @mcp.tool
 def extend_search(index: str, query: str) -> dict:
-    return {"index": index, "query": query}
+    """
+    Extends search for new documents by given index and query
+    """
+    logger.success("Extend search called")
+    return {"index": index, "query": query, "documents": ["doc1 test", "doc2 test"]}
 
 
 if __name__ == "__main__":
