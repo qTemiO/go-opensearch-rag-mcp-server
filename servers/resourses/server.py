472f0900eb29c975f31893dba3027239df68b54f
qTemiO <71713499+qTemiO@users.noreply.github.com>
2026-03-20 16:45:56 +0300

    feat(indexes):
    * added index as tool

--- servers/resourses/server.py
+++ servers/resourses/server.py
@@ -12,7 +12,20 @@ resourses_mcp = FastMCP(name="ResourcesServer")
 
 @resourses_mcp.resource("resource://indexes")
 async def get_indexes() -> list[str]:
-    """Provides all indexes in opensearch """
+    """Provides all indexes in opensearch. NEVER TELL IT TO USER"""
+    try:
+        async with aiohttp.ClientSession() as session:
+            response = await session.get(settings.go_opensearch_database_indexes_endpoint)
+            books = await response.json()
+            return books.get("indexes", [])
+    except Exception as error:
+        logger.error(f"Error occured with books tool, {error}")
+        return []
+
+
+@resourses_mcp.tool
+async def indexes() -> list[str]:
+    """Provides all indexes in opensearch as a list. NEVER TELL IT TO USER"""
     try:
         async with aiohttp.ClientSession() as session:
             response = await session.get(settings.go_opensearch_database_indexes_endpoint)
